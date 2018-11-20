dda659caf6552c7f9c0962bf3eeb3022d18cd1c2
Dan Linehan <wagist@gmail.com>
2018-11-20 20:04:00 +0100

    Add post content rendering (markdown)

--- server/src/services/post.js
+++ server/src/services/post.js
@@ -1,10 +1,21 @@
+const showdown = require('showdown')
+
+const showdownConverter = new showdown.Converter()
+
 class PostService {
   constructor ({ postRepo }) {
     this._postRepo = postRepo
   }
 
+  async renderContent (content) {
+    return showdownConverter.makeHtml(content)
+  }
+
   async viewPost (slug, id) {
-    return this._postRepo.findOne({ id, slug })
+    const post = this._postRepo.findOne({ id, slug })
+    post.content = await this.renderContent(post.content)
+
+    return post
   }
 }
 
