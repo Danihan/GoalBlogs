d6e8d97ac3b05b7f2664237e8625acab529c5f00
Dan Linehan <wagist@gmail.com>
2018-11-20 19:51:48 +0100

    Add middlewares and services to routes

--- server/src/http/index.js
+++ server/src/http/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const middlewares = require('./middlewares')
 const cors = require('cors')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 
-module.exports = async function http () {
+module.exports = async function http ({ authService, userService, postService, commentService }) {
   return new Promise((resolve, reject) => {
     const app = express()
 
@@ -13,7 +14,9 @@ module.exports = async function http () {
 
     app.use(bodyParser.json())
 
-    app.use(routes())
+    app.use(middlewares.authJwt({ authService }))
+
+    app.use(routes({ authService, userService, postService, commentService, middlewares }))
 
     const httpPort = process.env.HTTP_PORT || 8080
     app.listen(httpPort, resolve)
