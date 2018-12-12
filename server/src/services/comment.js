0a6ee7e515e60c0410080883ce3039f431fbe880
Dan Linehan <wagist@gmail.com>
2018-12-12 21:06:47 +0100

    Add comment rendering

--- server/src/services/comment.js
+++ server/src/services/comment.js
@@ -1,6 +1,13 @@
+const showdown = require('showdown')
+const showdownConverter = new showdown.Converter()
+
 class CommentService {
   constructor ({ commentRepo }) {
     this._commentRepo = commentRepo
+  }
+
+  async renderContent (content) {
+    return showdownConverter.makeHtml(content)
   }
 
   async getPostComments (postId) {
